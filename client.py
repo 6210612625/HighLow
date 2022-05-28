ec2cc80fc1cf29a2504d196cfe70c687571c45dc
6210612617 <6210612617@student,tu.ac.th>
2022-05-29 02:40:00 +0700

    com

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ from threading import Thread
 import tkinter as tk
 from tkinter import LEFT, RIGHT, ttk
 from turtle import left, right
-
+from PIL import Image, ImageTk
 
 
 
@@ -18,14 +18,15 @@ BUFFER_SIZE = 1024
 
 s.connect(('127.0.0.1', 7077))
 
-rounds = 1
 end_game = False
 player=2
 
 count_high = False
 count_low = False
-sum_ans = 0
 
+stb = True
+rounds = 0
+sum_ans = 0
 
 
 def win(player):
@@ -45,37 +46,51 @@ def lost(player):
 def check():    
     global rounds
     global end_game
-    if(rounds <=3):
-        #check p1 is correct answer -> win(player)
-        if sum_ans > 9:
-            if(count_high == True):
-                rounds+=1
-            else:
-                send_ans("lost")
-                lost(player)
-                
-        else:
-            if(count_low == True):
-                rounds+=1
-            else:
-                send_ans("lost")
-                lost(player)
-                
+    global correct
+    ans = str(sum_ans)
+    if(count_high == False and count_low == False):
+        pass
     else:
-        send_ans("draw")
-        draw()
-        
+        if(rounds <=3):
+            #check p1 is correct answer -> win(player)
+            if sum_ans > 9:
+                if(count_high == True):
+                    
+                    messagebox.showinfo(title="Congratulation", message='correct! answer is '+ ans)
+                    startgame()
+                    correct = True
+                else:
+                    send_ans("lost")
+                    lost(player)
+                    
+                    
+                    
+            else:
+                if(count_low == True):
+                    messagebox.showinfo(title="Congratulation", message='correct! answer is '+ ans)
+                    startgame()
+                    correct = True
+                else:
+                    send_ans("lost")
+                    lost(player)
+                    
+                    
+        else:
+            send_ans("draw")
+            draw()
     
             
             
         #check p1 is uncorrect answer -> lost(player)
         #if rounds = 3 -> draw()
 def handle(type):
-
     if(type == "lost"):
         win(player)
-    else:
+    elif(type == 'darw'):
         draw()
+    else:
+        pass
+    
 def handle_ran(ans):
     global sum_ans
     sum_ans = ans
@@ -84,22 +99,75 @@ def handle_ran(ans):
     
 def restart():
     global rounds
-    rounds = 1
+    global count_low
+    global count_high
+    rounds = 0
+    count_high =False
+    count_low =False
 
-
+def startgame():
+    global rounds
+    if (rounds <= 3):
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+        dice3= random.randint(1,6)
+        
+        ans=[dice1, dice2 ,dice3]
+        
+        global sum_ans
+        sum_ans=sum(ans)
+        
+        
+        
+        rounds+=1
+        mylabel.config(text = sum_ans)
+        rnd = str(rounds)
+        label_round.config(text = "ROUND : " + rnd)
+    start_button.configure(state=DISABLED)
 #def quit():
  #   s.close()
   #  wind.destroy()
 
-# root window
 root = tk.Tk()
-root.geometry('300x200')
+#root.geometry('420x390')
+root.geometry('1000x1000')
 root.resizable(False, False)
 root.title('HIGH-LOW')
+root.configure(bg = "LightPink")
+
+global my_label
+global label_round
+label = tkinter.Label(root, text = "\n\n  HIGH-LOW GAME! \n\n", font=("Arial", 20), 
+            bg = "LightPink", fg = "deep pink").pack()
+
+label = tkinter.Label(root, text =( "hi" ), font=("Arial", 20), 
+            bg = "LightPink", fg = "deep pink").pack()
+
+    
+
+label_round = tk.Label(root, text = "Hello World", bg = "red")
+label_round.pack(padx = 5, pady = 10)
+mylabel = tk.Label(root, text = "Hello World", bg = "red")
+mylabel.pack(padx = 5, pady = 10)
 
 
+mybutton = tk.Button(root, text = "Click Me", command = startgame)
+mybutton.pack(padx = 5, pady = 10)
+#IMG
+dicesImg = Image.open(r"dices.png").resize((100, 100))
+
+img = PhotoImage(file="dices.png")      
+
+label_img = Label(root, image=img,bg = "LightPink")
+label_img.pack(padx=10,pady=10)
+
+#def เพื่อให้ปุ่มหาย
+def hide_me(event):
+    #event.widget.pack_forget()
+    #event.config
+    start_button.configure(state=DISABLED)
+    
 # high button
-
 high_button = ttk.Button(
     root,
     text='HIGH',
@@ -129,13 +197,39 @@ low_button.pack(
     side=RIGHT,
     expand=True
 )
+start_button = ttk.Button(
+    root,
+    text='START',
+    command=lambda: startgame()
+    
+)
 
+start_button.pack(
+    ipadx=5,
+    ipady=5,
+    expand=True
+    
+)
+
+restart_button = ttk.Button(
+    root,
+    text='RESTART',
+    command=lambda: restart()
+)
+# ขนาดของการแสดงปุ่ม
+restart_button.pack(
+    ipadx=5,
+    ipady=5,
+    expand=True
+)
+restart_button.place(x=110, y=130)
 
 def High():
     global count_high
     count_high = True
     global count_low
     count_low = False
+    check()
 
 
 def Low():
@@ -143,8 +237,23 @@ def Low():
     count_low = True
     global count_high
     count_high = False
+    check()
+
+def restart():
+    global rounds
+    global count_low
+    global count_high
+    rounds = 0
+    count_high =False
+    count_low =False
+    startgame()
+    #s.close()
+    #handle_client()
+
+
 
 root.mainloop()
+
 
     
 def send_ans(output):
@@ -152,21 +261,35 @@ def send_ans(output):
     output = output.encode()
     s.send(output)
 
-def receive_ans(output):
+""" def receive_ans(output):
     output = output.decode()
-    output = str(output)
+    output = int(output)
     handle(output)
     
 def receive_ran(output):
     output = output.decode()
     output = str(output)
-    handle_ran(output)
+    handle_ran(output) """
+    
+def receive_message():
+    while True:
+        p = s.recv(10)
+        apply(p)
+def apply(input):
+    input = input.decode()
+    input = str(input)
+    incheck(input)
+def incheck(input):
+    digi = input.isdigit()
+    if(digi ==  True):
+        handle_ran(input)
+    else:
+        handle(input)
+
+
     
 
-
-    
-
-receive = Thread(target=receive_ans)
+receive = Thread(target=receive_message)
 receive.start()
 
 
