508707e1920065596c0bac33c70147fa7141f0a5
Anuradee Bundasak <6210612625@cn103>
2022-05-28 21:04:59 +0700

    second update

--- button.py
+++ button.py
@@ -1,6 +1,8 @@
+from itertools import count
 import tkinter as tk
-from tkinter import LEFT, RIGHT, ttk
+from tkinter import BOTTOM, CENTER, LEFT, RIGHT, Place, ttk
 from turtle import left, right
+from xml.etree.ElementTree import tostring
 from PIL import Image, ImageTk
 
 # root window
@@ -11,37 +13,69 @@ root.title('HIGH-LOW')
 
 
 # high button
+count_low = False
+count_high = False
+
+def High():
+    global count_high
+    count_high = True
+    global count_low
+    count_low = False
+
+
+def Low():
+    global count_low
+    count_low = True
+    global count_high
+    count_high = False
+
+
 high_button = ttk.Button(
     root,
     text='HIGH',
-    command=lambda: root.High()
-    
+    command=lambda: High() 
+)
+high_button.pack(
+    ipadx=5,
+    ipady=5,
+    side=LEFT,
+    expand=True,
 )
 
 # low button
 low_button = ttk.Button(
     root,
     text='LOW',
-    command=lambda: root.High()
+    command=lambda: Low()
 )
 
 # ขนาดของการแสดงปุ่ม
-high_button.pack(
-    ipadx=5,
-    ipady=5,
-    side=LEFT,
-    expand=True,
-    
-)
-
 low_button.pack(
     ipadx=5,
     ipady=5,
     side=RIGHT,
     expand=True
 )
-root.mainloop()
 
+reset_button = ttk.Button(
+    root,
+    text='RESET',
+    command=lambda: Reset()
+)
+
+# ขนาดของการแสดงปุ่ม
+reset_button.pack(
+    ipadx=5,
+    ipady=5,
+    expand=True
+)
+
+# reset_button.place(x=110, y=130)
+reset_button.grid(column=2, row=3)
+
+print(count_high)
+root.mainloop()
+print('2' , count_high)
 '''
 pImg = Image.open(r"S_48775216.jpg").resize((100, 100))
 lImg = Image.open(r"year3-2.jpg").resize((100, 100))
